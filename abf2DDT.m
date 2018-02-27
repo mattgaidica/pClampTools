053546d968124d2b70b7151d09469b3324d8d4ae
Matt Gaidica <matt@gaidi.ca>
2018-02-27 13:08:59 -0500

    Reshaping ABF file

--- abf2DDT.m
+++ abf2DDT.m
@@ -2,5 +2,6 @@ function saveFile = abf2DDT(filename)
 
 [pathstr, name, ext] = fileparts(filename);
 [time, dt, data_i, data_v, cell_name] = loadVclampAbf(filename);
+data_v_reshape = reshape(data_v,[1 size(data_v,1)*size(data_v,2)]);
 saveFile = fullfile(pathstr,[name,'.ddt']);
-ddt_write_v(saveFile,1,length(data_v),1/(dt/1000),data_v);
+ddt_write_v(saveFile,1,length(data_v_reshape),1/(dt/1000),data_v_reshape);
